de56b5220ff1f3171a71b50ae9780f7e686d29ac
Sabyasachi Mukhopadhyay <sabya39@gmail.com>
2023-10-18 18:32:24 +0530

    Adding new classifier

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,7 +4,7 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
-#Pushing changes to run UT
+#Pushing changes to correct UT
 
 app = FastAPI(title="Predicting Wine Class with batching")
 
